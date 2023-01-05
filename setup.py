94b20a11f10e39c9d4d60326f3ae58e16ec6805e
Baptiste Grenier <baptiste.grenier@egi.eu>
2023-01-05 17:07:37 +0100

    Bump version and fix URL.

--- setup.py
+++ setup.py
@@ -3,13 +3,13 @@
 from distutils.core import setup
 
 setup(name='glue-validator',
-      version='1.0.2',
+      version='2.0.25',
       description='GLUE 2.0 Valiation',
       long_description ='Validation scripts for an LDAP based information system using the GLUE 2.0 schema',
       author='Laurence Field',
       author_email='Laurence.Field@cern.ch',
       license='Apache 2.0',
-      url='http://cern.ch/glue',
+      url='https://github.com/EGI-Federation/glue-validator',
       scripts = ['bin/glue-validator'],
       packages=['glue2','glue1','egi-glue2','validator'],
       package_dir = {'': 'lib'},
