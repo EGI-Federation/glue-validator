402ac17eedb18bbd372b90f96378f0e473e5e2df
Baptiste Grenier <baptiste.grenier@egi.eu>
2023-03-21 11:55:38 +0100

    setup.py: style

--- setup.py
+++ setup.py
@@ -1,16 +1,15 @@
-#!/usr/bin/env python
-
 from distutils.core import setup
 
-setup(name='glue-validator',
-      version='2.0.25',
-      description='GLUE 2.0 Valiation',
-      long_description ='Validation scripts for an LDAP based information system using the GLUE 2.0 schema',
-      author='Laurence Field',
-      author_email='Laurence.Field@cern.ch',
-      license='Apache 2.0',
-      url='https://github.com/EGI-Federation/glue-validator',
-      scripts = ['bin/glue-validator'],
-      packages=['glue2','glue1','egi-glue2','validator'],
-      package_dir = {'': 'lib'},
-      )
+setup(
+    name="glue-validator",
+    version="2.0.25",
+    description="GLUE 2.0 Valiation",
+    long_description="Validation scripts for an LDAP based information system using the GLUE 2.0 schema",  # noqa: 501
+    author="Laurence Field",
+    author_email="Laurence.Field@cern.ch",
+    license="Apache 2.0",
+    url="https://github.com/EGI-Federation/glue-validator",
+    scripts=["bin/glue-validator"],
+    packages=["glue2", "glue1", "egi-glue2", "validator"],
+    package_dir={"": "lib"},
+)
